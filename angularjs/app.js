b8d5fef0b1d5eb78b8f19816596331c9cf0c4032
Wellington Silva Melo <tomwel6@hotmail.com>
2018-03-29 17:14:51 -0300

    mudei o app.js

--- angularjs/app.js
+++ angularjs/app.js
@@ -3,7 +3,7 @@
 
     console.log("[ Inicializando módulo principal do AngularJs... ]");
 
-    angular.module("app", ["ui.bootstrap","uiMaskTom","uiAccordionTom"]);
+    angular.module("app", ["uiMaskTom","uiAccordionTom"]);
 
     console.log("[ Módulo principal do AngularJs Inicializado. ]");
     
